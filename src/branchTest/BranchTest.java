2aa4d92a9d44875406ae32e5e6c861159fc5cf8e
봄 <jxxbom@gmail.com>
2019-04-16 00:56:40 +0900

    start branch test

---
+++ src/branchTest/BranchTest.java
@@ -0,0 +1,24 @@
+package branchTest;
+
+public class BranchTest {
+
+	public static void main(String[] args) {
+		
+		Branch branch=new Branch();
+		branch.create();
+
+	}
+
+}
+
+class Branch {
+
+	public Branch() {
+		System.out.println("Start Branch Test");
+	}
+	
+	public void create() {
+		
+	}
+	
+}
