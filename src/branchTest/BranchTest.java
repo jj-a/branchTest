fca3a2c5e335dfe0e914bd80a5a86c45f0733646
Jun <vagrantsj@gmail.com>
2019-04-16 12:41:13 +0900

    Merge branch 'feature/add1' of https://github.com/jj-a/branchTest into feature/add1
    # Conflicts:
    #	bin/branchTest/Branch.class
    #	src/branchTest/BranchTest.java

--- src/branchTest/BranchTest.java
+++ src/branchTest/BranchTest.java
@@ -31,6 +31,21 @@ class Branch {
 	public void add1(int num) {
 		System.out.println("Add push 1-1");
 		System.out.println("석준' 수정");
+=======
+		System.out.println("Start Branch Test 테스트");
+	}
+	
+	public void create() {
+		System.out.println("Create Feature Branch 테스트");
+	}
+	
+	public void add1() {
+		System.out.println("Add push 1 테스트");
+	}
+	
+	public void add1(int num) {
+		System.out.println("Add push 1-1 테스트");
+>>>>>>> c88a195ef7be8077d9c25b3144a6146264ecaf19
 	}
 	
 }
